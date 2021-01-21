eda5dbf6d33e8e01142d97b3f4d7392a3cc4a3ee
MyProcast <71037005+MyProcast@users.noreply.github.com>
2021-01-21 03:01:49 -0500

    Actual Commit

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private GameObject turretToBuild;
+
+    //Shop script uses these two variables but its acutal game object assinment is stored in the Game Master
+    public GameObject standardTurretPrefab;
+    public GameObject heavyTurretPrefab;
+
+    //References itself so other scripts can use this all of this and vise versa
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+    public void SetTurretToBuild(GameObject turret)
+    {
+        turretToBuild = turret;
+    }
+}
